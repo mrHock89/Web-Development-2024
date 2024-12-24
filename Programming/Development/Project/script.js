55addf39627995bd3b6d3a45018083f10da2a7a3
Mr. Hock <manojmondal9900@gmail.com>
2024-12-24 12:33:12 +0530

    fixed minor bug fix like "00.5+3" will give answer

--- Programming/Development/Project/script.js
+++ Programming/Development/Project/script.js
@@ -2,28 +2,38 @@ let display = document.getElementById("inputBox");
 let buttons = document.querySelectorAll("button");
 
 let buttonArray = Array.from(buttons);
-let string = " ";
+let string = "";
+
+// sanitize the string
+const sanitizeInput = (input) => {
+  if (/^0[0-9]/.test(input)) {
+    input = input.replace(/^0+/, "");
+  }
+  return input;
+};
 // loop for every value
 buttonArray.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "DEL") {
+    const btnValue = e.target.innerHTML;
+    if (btnValue == "DEL") {
       string = string.substring(0, string.length - 1);
-      display.value = string;
-    } else if (e.target.innerHTML == "AC") {
+    } else if (btnValue == "AC") {
       string = "";
-      display.value = string;
-    } else if (e.target.innerHTML == "=") {
-      string = eval(string);
-      display.value = string;
-    } else if (e.target.innerHTML == "÷") {
+    } else if (btnValue == "=") {
+      try {
+        string = eval(string).toString();
+      } catch (error) {
+        string = "Error";
+      }
+    } else if (btnValue == "÷") {
       string += "/";
-      display.value = string;
     } else {
       // for storing the button value
-      string += e.target.innerHTML;
+      string += btnValue;
+      string = sanitizeInput(string);
       // for displaying button values
-      display.value = string;
     }
+    display.value = string;
     // console.log(e.target.innerHTML);
   });
 });
